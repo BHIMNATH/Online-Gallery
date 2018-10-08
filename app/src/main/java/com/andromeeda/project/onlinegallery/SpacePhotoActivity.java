cc821e04d469cbe32984c0c796e581447309ac0d
Aswin Bhim Nath <aswismail@gmail.com>
2018-10-08 16:31:26 +0530

    Image Fullscreen Activity

---
+++ app/src/main/java/com/andromeeda/project/onlinegallery/SpacePhotoActivity.java
@@ -0,0 +1,13 @@
+package com.andromeeda.project.onlinegallery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpacePhotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_space_photo);
+    }
+}
