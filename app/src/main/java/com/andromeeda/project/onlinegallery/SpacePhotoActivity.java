75d27c6cecdb7261f090a1ce68a16587b74803b1
Aswin Bhim Nath <aswismail@gmail.com>
2018-10-08 16:34:21 +0530

    Image Fullscreen Activity

--- app/src/main/java/com/andromeeda/project/onlinegallery/SpacePhotoActivity.java
+++ app/src/main/java/com/andromeeda/project/onlinegallery/SpacePhotoActivity.java
@@ -2,12 +2,29 @@ package com.andromeeda.project.onlinegallery;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 public class SpacePhotoActivity extends AppCompatActivity {
+
+    public static final String EXTRA_SPACE_PHOTO = "SpacePhotoActivity.SPACE_PHOTO";
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_space_photo);
+
+        mImageView = (ImageView) findViewById(R.id.image);
+        SpacePhoto spacePhoto = getIntent().getParcelableExtra(EXTRA_SPACE_PHOTO);
+
+        Glide.with(this)
+                .load(spacePhoto.getUrl())
+                .asBitmap()
+                .error(R.drawable.ic_launcher_background)
+                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                .into(mImageView);
     }
 }
